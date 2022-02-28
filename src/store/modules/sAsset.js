8889e798744d43e7a30b2931d7f014a72aa94b68
prado <prado.com@protonmail.com>
2022-02-28 18:51:37 +0800

    add asset dashboard data framework, without fill with data

--- src/store/modules/sAsset.js
+++ src/store/modules/sAsset.js
@@ -3,19 +3,33 @@ const sAsset = {
     state: {
         name: "sMatic",
         contract:{
+            ETH: "",
             DAI: "",
             OHM: "",
             Treasury: "",
             Calc: "",
-            distributor:"",
+            Distributor:"",
             sOHM: "",
-            staking: "",
-            staking_warmup:"",
-            staking_helper:"",
-            DAI_bond: "",
-            consensus_pool: ""
+            Staking: "",
+            Staking_Warmup:"",
+            Staking_Helper:"",
+            DAI_Bond: "",
+            ConsensusPool: "",
+            DAO: "",
+            Swap_Router: "",
+            OHM_DAI_LP: ""
         },
-
+        DAIsPerOHM: "",
+        currentIndex: "",
+        OHMTotalSupply: "",
+        OHMBalanceOfStaking: "",
+        DAIBalanceOfTreasury: "",
+        OHMDAILPBalanceOfTreasury: "",
+        OHMDAILPTotalSupply: "",
+        DAIBalanceOfOHMDAILP: "",
+        OHMBalanceOfDAO: "",
+        epochDistribute: "",
+        stakingContractBalance: "",
     },
     mutations: {
         SET_NAME: (state, name) => {
@@ -23,7 +37,40 @@ const sAsset = {
         },
         SET_CONTRACT:(state, contract)=>{
             state.contract=contract;
-        }
+        },
+        SET_DAIS_PER_OHM: (state, val) => {
+            state.DAIsPerOHM = val;
+        },
+        SET_CURRENT_INDEX: (state, val) => {
+            state.currentIndex = val;
+        },
+        SET_OHM_TOTAL_SUPPLY: (state, val) => {
+            state.OHMTotalSupply = val;
+        },
+        SET_OHM_BALANCE_OF_STAKING: (state, val) => {
+            state.OHMBalanceOfStaking = val;
+        },
+        SET_DAI_BALANCE_OF_TREASURY: (state, val) => {
+            state.DAIBalanceOfTreasury = val;
+        },
+        SET_OHM_DAI_LP_BALANCE_OF_TREASURY: (state, val) => {
+            state.OHMDAILPBalanceOfTreasury = val;
+        },
+        SET_OHM_DAI_LP_TOTAL_SUPPLY: (state, val) => {
+            state.OHMDAILPTotalSupply = val;
+        },
+        SET_DAI_BALANCE_OF_OHM_DAI_LP: (state, val) => {
+            state.DAIBalanceOfOHMDAILP = val;
+        },
+        SET_OHM_BALANCE_OF_DAO: (state, val) => {
+            state.OHMBalanceOfDAO = val;
+        },
+        SET_EPOCH_DISTRIBUTE: (state, val) => {
+            state.epochDistribute = val;
+        },
+        SET_STAKING_CONTRACT_BALANCE: (state, val) => {
+            state.stakingContractBalance = val;
+        },
     },
     actions: {
         set_name({ commit }, name) {
